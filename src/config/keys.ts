e5e435ab492820a75eb134cb5a20e00fcdac1651
Alonso <alo@Alonsos-MacBook-Pro.local>
2022-02-20 12:30:44 -0600

    initial commit

---
+++ src/config/keys.ts
@@ -0,0 +1,9 @@
+export default {
+    mongoURI: 'mongodb+srv://alonsoup:alonsoup123@cluster0.u6nzh.mongodb.net/sample_airbnb?retryWrites=true&w=majority'
+}
+// alonsoup
+// alonsoup123
+// Ok. Me quedé en que no me puedo conectar a la base de datos. Ya puse mi ip, ya creé un nuevo usuario con un chingo de permisos, ya revisé el nombre de la base que quiero usar.
+// Quiero intentar conectarme por medio de mongosh, pero se está tardando años en instalar
+//
+//
